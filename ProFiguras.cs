36a299c80c904a5565892fa6de0647ebfe0a728d
K3120 <MarKei123>
2020-12-11 20:21:19 +0100

    Agregar archivos de proyecto.

---
+++ ProFiguras.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poyecto_2
+{
+    internal class ProFiguras
+    {
+        double lado = 0;
+        double radio = 0;
+        double altura = 0;
+        double area = 0;
+
+
+
+        internal void areaCuadrado()
+        {
+
+            Console.WriteLine("Ingresa el valor del lado del CUADRADO:");
+            lado = Convert.ToDouble(Console.ReadLine());
+            area = lado * lado;
+            Console.WriteLine("El área del cuadrado es:" + area);
+
+        }
+        internal void areaCirculo()
+        {
+
+            Console.WriteLine("Ingresa el valor del radio del CÍRCULO:");
+            radio = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * Math.Pow(radio, 2);
+            Console.WriteLine("El radio del círculo es:" + area);
+
+        }
+
+        internal void areaRectángulo()
+        {
+
+            Console.WriteLine("Ingresa el valor de la base del RECTÁNGULO:");
+            lado = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa el valor de la altura del RECTÁNGULO:");
+            altura = Convert.ToDouble(Console.ReadLine());
+            area = lado * altura;
+            Console.WriteLine("El área del rectángulo es:" + area);
+
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Poyecto_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int contador = 1;
+
+                ProFiguras figuras = new ProFiguras();
+                for (int i = 0; i < contador; i++)
+                {
+
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("Calcula el área de una figura <S/N>");
+                    string opcion = Console.ReadLine();
+                    if (opcion.ToUpper() == "S")
+                    {
+                        contador++;
+                        Console.WriteLine(" Elije una Figura: \t" +
+                            "1 CUADRADO \t" + "2 CÍRCULO \t" + "3 RECTÁNGULO");
+                        char figuraSeleccionada = Convert.ToChar(Console.ReadLine());
+
+                        switch (figuraSeleccionada)
+                        {
+                            case '1': figuras.areaCuadrado(); break;
+                            case '2': figuras.areaCirculo(); break;
+                            case '3': figuras.areaRectángulo(); break;
+
+                            default:
+                                Console.WriteLine("Esa opción no está Disponible");
+                                break;
+                        }
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gracias, enseguida se Cerrará la Aplicación");
+                        Console.WriteLine("...3");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("..2");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine(".1");
+                        System.Threading.Thread.Sleep(1000);
+                        Environment.Exit(0);
+                        //return;
+
+                    }
+                    Console.ReadLine();
+
+                }
+               
+               
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("SE HA DETECTADO UN ERROR: \t" + x.Message);
+                Console.ReadLine();
+
+            }
+
+            
+
+
+
+        }
+    }
+}
